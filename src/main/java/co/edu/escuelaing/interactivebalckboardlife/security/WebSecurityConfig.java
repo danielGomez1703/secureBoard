dcc88c3c05c328d41ee682508f6af8fcd62ef7eb
Daniel Gomez <danipipe1703@gmail.com>
2020-07-10 13:04:18 -0500

    Seguridad web - websocket

---
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/security/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.interactivebalckboardlife.security;
+
+/**
+ *
+ * @author danip
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user
+                = User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        UserDetails user2
+                = User.withDefaultPasswordEncoder()
+                        .username("user2")
+                        .password("password2")
+                        .roles("USER")
+                        .build();
+        InMemoryUserDetailsManager imudm =  new InMemoryUserDetailsManager();
+        imudm.createUser(user);
+        imudm.createUser(user2);
+
+        return imudm;
+    }
+}
